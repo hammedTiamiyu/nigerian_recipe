a7a839bc0eed274514f546be914b35fa8e85aa71
hammedTiamiyu <hammed.tiamiyu@grandiasolutions.com>
2023-08-29 13:21:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import data from './data'
+import './App.css'
+import './index'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {data.map(( ss, index) => (
+      <div>
+      <section>
+          <h2 className="text-3xl font-bold">{ss.recipeName}</h2>
+      </section>
+      <section className="flex justify-center items-center my-8">
+          <img src={ss.imageUrl} alt={ss.recipeName}/>
+      </section>
+      <section className='my-8'>
+          <h3 className="text-3xl font-bold">Ingredients</h3>
+          <p className='flex justify-center items-center'>{ss.ingredients}</p>
+      </section>
+      <section>
+          <h3 className="text-3xl font-bold">Method Of Preparation</h3>
+          <p>{ss.note}</p>
+      </section>
+      
+     </div>
+    ))}
+     {/* </section> */}
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,17 @@
+ const data = [
+    {
+        'recipeName': 'Nigeria Jollof Rice',
+        'note': 'Jollof, or jollof rice, is a rice dish from West Africa. The dish is typically made with long-grain rice, tomatoes, onions, spices, vegetables and meat in a single pot, although its ingredients and preparation methods vary across different regions.',
+        'imageUrl': 'https://unsplash.com/photos/Qtd5z7g4thc/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8am9sbG9mJTIwcmljZXxlbnwwfHx8fDE2OTMxNjI0NzN8MA&force=true',
+        'ingredients' : "Tomatoes, Brown onion, Red onion, Tomato puree, Vegetable oil, Curry powder, Dried Thyme, White pepper, Bay leaves, Scotch bonnet, Stock cubes, Chicken broth, Salt, Plum or salad",
+
+        'instruction': ["1. It is key to use parboiled rice so that you don’t end up with mushy rice in the end.",
+                    "2. When cooking the tomato stew, you can tell when it is done because the oil will rise to the top. It will be a deep red color and the bitter taste of the tomatoes has been cooked out.",
+                    "3. Reserve 1/4th of the tomato mixture and add it at the end to help prevent the rice from burning. Also, it gives the rice that beautiful red color we all know and love.",
+                    "4. Cover the top of the pot with plastic wrap before putting the lid on to help keep all the steam in the pot."
+        ]
+
+    }
+]
+
+export default data
