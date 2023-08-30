43d280b2bf9d47676315a61e3a483604854522d0
hammedTiamiyu <hammed.tiamiyu@grandiasolutions.com>
2023-08-30 10:49:47 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -19,11 +19,14 @@ function App() {
           <h3 className="text-3xl font-bold">Ingredients</h3>
           <p className='flex justify-center items-center'>{ss.ingredients}</p>
       </section>
-      <section>
-          <h3 className="text-3xl font-bold">Method Of Preparation</h3>
+      <section className='my-8'>
+          <h3 className="text-3xl font-bold">About The Recipe</h3>
           <p>{ss.note}</p>
       </section>
-      
+      <section>
+          <h3 className="text-3xl font-bold">Method Of Preparation</h3>
+          <p>{ss.instruction}</p>
+      </section>
      </div>
     ))}
      {/* </section> */}
